1d85ef8dd0c2a229c7e1472404d20415b5015da9
Simone D'Avico <simone.davico@welld.ch>
2021-03-21 18:46:39 +0100

    feat(geo): open maps from friend list item

--- src/components/FriendListItem.tsx
+++ src/components/FriendListItem.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { createOpenLink } from 'react-native-open-maps';
 import { spacing } from '../design';
 import { FriendWithDistance } from '../features/friends/types';
 import intl from '../intl';
+import Button from './Button';
 
 interface FriendListItemProps {
   friend: FriendWithDistance;
@@ -10,24 +12,36 @@ interface FriendListItemProps {
 }
 
 const FriendListItem: React.FC<FriendListItemProps> = ({ friend, onPress }) => {
+  const { geo } = friend.address;
   return (
     <TouchableOpacity style={styles.item} onPress={() => onPress(friend)}>
-      <Text>{friend.name}</Text>
-      {friend.distanceInKm !== undefined ? (
-        <Text>
-          is {intl.formatNumber(friend.distanceInKm, { unit: 'kilometers' })}{' '}
-          away
-        </Text>
-      ) : null}
+      <View style={styles.content}>
+        <Text>{friend.name}</Text>
+        {friend.distanceInKm !== undefined ? (
+          <Text>
+            is {intl.formatNumber(friend.distanceInKm, { unit: 'kilometers' })}{' '}
+            away
+          </Text>
+        ) : null}
+      </View>
+      <Button
+        hitSlop={{ top: 8, left: 8, bottom: 8, right: 8 }}
+        onPress={createOpenLink({ latitude: geo.lat, longitude: geo.lng })}>
+        <Text>Maps</Text>
+      </Button>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   item: {
+    borderBottomWidth: 1,
+    flexDirection: 'row',
     paddingHorizontal: spacing.s2,
     paddingVertical: spacing.s5,
-    borderBottomWidth: 1,
+  },
+  content: {
+    flex: 1,
   },
 });
 
