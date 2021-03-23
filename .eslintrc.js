120c28e789544b1c91f8daefa00efe8fd720515f
Simone D'Avico <simone.davico@welld.ch>
2021-03-23 11:04:24 +0100

    test(e2e): add detox configuration for e2e tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:jest/recommended'],
+  overrides: [
+    {
+      files: ['e2e/**/*.js'],
+      env: {
+        jest: true,
+        'jest/globals': true,
+      },
+      globals: {
+        device: true,
+        element: true,
+        by: true,
+        waitFor: true,
+      },
+    },
+  ],
 };
